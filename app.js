4a29a2ba3e68341ca62bf3fcf12017cb20c37e97
rishi <rishirewadkar@gmail.com>
2020-05-16 22:28:39 +0530

    init commit

--- app.js
+++ app.js
@@ -71,6 +71,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'public/index.html'));
+});
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
